5b183739c2514db970d05410e7a658febf706857
asnganje <abd.nganje@gmail.com>
2023-12-10 19:39:04 +0300

    get images and pass the content as images props to ImageList

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import SearchBar from "./components/SearchBar";
 import { fetchImages } from "./api/api";
+import { useState } from "react";
+import ImageList from "./components/ImageList";
 
 
 
 const App = () => {
 
+    const [images, setImages] = useState([])
 
     const termHandler = (term) => {
         const results = fetchImages(term)
+        setImages(results)
     }
     return(
         <div>
             <SearchBar termHandler = {termHandler} />
+            <ImageList images = {images}/>
         </div>
     )
 }
